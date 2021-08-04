8f091f96c46847b1798b5b9c20008c05b3286117
Ryu4512 <88323194+Ryu4512@users.noreply.github.com>
2021-08-04 22:00:11 +0900

    Revert "Revert "gui""
    This reverts commit 49478a924678ca11b612bff2cac646371b2d328a.

---
+++ 18_MiniProject1/src/com/kh/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.run;
+
+import com.kh.view.MainFrame;
+import com.kh.view.SignUp;
+import com.kh.view.SignUp2;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new MainFrame();
+//		new SignUp();
+//		new SignUp2();
+	}
+
+}
