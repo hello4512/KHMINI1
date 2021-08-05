33bcd588508ad68271c34dc9c0597379ae851090
orzsasin <80640024+orzsasin@users.noreply.github.com>
2021-08-05 16:07:34 +0900

    kwang jung
    로그인 후 화면, 관리자 화면 버튼눌르면 액션 까지 추가 한거에요

---
+++ test/src/test/com/kh/test/run/Run.java
@@ -0,0 +1,14 @@
+package test.com.kh.test.run;
+
+import test.com.kh.test.view.MainFrame;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		// LoginAfterPage lap = new LoginAfterPage();
+		// ManagerPage mp = new ManagerPage();
+		new MainFrame();
+	}
+
+}
---
+++ test/src/test/com/kh/test/view/LoginAfterPage.java
@@ -0,0 +1,161 @@
+package test.com.kh.test.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class LoginAfterPage extends JPanel{
+	private MainFrame mf;
+	private JPanel mainPage;
+	private Image img;
+	
+	public LoginAfterPage(MainFrame mf) {
+		this.mf = mf;
+		this.mainPage = this;
+		
+		// 로그인 후 화면
+		
+		// up JPanel
+		JPanel upPanel = new JPanel();
+		upPanel.setPreferredSize(new Dimension(840, 80));
+		upPanel.setBackground(Color.darkGray);
+			
+		// JLabel "no."
+		JLabel nojl = new JLabel("no."/*OO 입니다*/);
+		upPanel.add(nojl);
+		nojl.setFont(new Font("고딕", Font.BOLD, 30));
+		nojl.setForeground(Color.white);
+		nojl.setPreferredSize(new Dimension(560, 60));
+		
+		/*// JLabel "회원번호 or 비회원 번호"
+		JLabel nojl2 = new JLabel("OO 입니다");
+		upPanel.add(nojl2);
+		nojl2.setFont(new Font("고딕", Font.BOLD, 30));
+		nojl2.setForeground(Color.white);
+		nojl2.setPreferredSize(new Dimension(260, 60));
+		*/
+		
+		// JButton "자리이동"
+		JButton move = new JButton("자리 이동 >>");
+		move.setBackground(Color.darkGray);
+		upPanel.add(move);
+		move.setPreferredSize(new Dimension(240, 60));
+		move.setFont(new Font("고딕", Font.BOLD, 20));
+		move.setForeground(Color.yellow);
+		move.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "자리이동 페이지로");			
+			}});
+				
+		// left JPanel 
+		JPanel leftPanel = new JPanel();
+		leftPanel.setPreferredSize(new Dimension(840, 720));
+		leftPanel.setBackground(Color.black);
+		leftPanel.add(upPanel);
+		leftPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+				
+		// leftPanel JLabel ID 
+		JLabel info = new JLabel("ID : ");
+		leftPanel.add(info);
+		info.setFont(new Font("고딕", Font.BOLD, 30));
+		info.setForeground(Color.white);
+		info.setPreferredSize(new Dimension(800, 80));
+		
+		
+		// leftPanel JLabel 회원등급
+		JLabel rank = new JLabel("회원등급 : ");
+		leftPanel.add(rank);
+		rank.setFont(new Font("고딕", Font.BOLD, 30));
+		rank.setForeground(Color.white);
+		rank.setPreferredSize(new Dimension(800, 80));
+		
+		// leftPanel JLabel 남은시간
+		JLabel time = new JLabel("남은시간 : ");
+		leftPanel.add(time);
+		time.setFont(new Font("고딕", Font.BOLD, 60));
+		time.setForeground(Color.white);
+		time.setPreferredSize(new Dimension(800, 160));
+		
+		
+		// 현재 시간
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date today = new Date();
+		
+		// leftPanel JLabel 시작시간
+		JLabel starttime = new JLabel("시작시간 : " + format.format(today));
+		leftPanel.add(starttime);
+		starttime.setFont(new Font("고딕", Font.BOLD, 30));
+		starttime.setForeground(Color.white);
+		starttime.setPreferredSize(new Dimension(800, 80));
+			
+		
+		// leftPanel JLabel 사용시간
+		JLabel usagetime = new JLabel("사용시간 : ");
+		leftPanel.add(usagetime);
+		usagetime.setFont(new Font("고딕", Font.BOLD, 30));
+		usagetime.setForeground(Color.white);
+		usagetime.setPreferredSize(new Dimension(800, 80));
+		
+		// JButton "사용 종료"
+		JButton exit = new JButton("사용 종료");
+		exit.setBackground(Color.red);
+		leftPanel.add(exit);
+		exit.setPreferredSize(new Dimension(180, 60));
+		exit.setFont(new Font("고딕", Font.BOLD, 30));
+		exit.setForeground(Color.white);
+		exit.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "사용을 종료 합니다.");			
+			}});
+				
+		
+		// right JPanel 생성
+		JPanel rightPanel = new JPanel();
+		rightPanel.setPreferredSize(new Dimension(400, 720));
+		rightPanel.setBackground(Color.gray);
+		Image icon = new ImageIcon("image/raccoon.jpg").getImage().getScaledInstance(340, 520, 0);
+		// JLabel 생성하여 Image 설정
+		JLabel label = new JLabel(new ImageIcon(icon));
+		rightPanel.add(label);
+		// 메인 페이지에서 클릭이 일어났을 때 이벤트 감지
+		// this.addMouseListener(new MyMouseAdapter());
+		
+		// right JPanel "메뉴 주문" button
+		JButton menubutton = new JButton("메뉴 주문");
+		menubutton.setBackground(Color.blue);
+		rightPanel.add(menubutton);
+		menubutton.setPreferredSize(new Dimension(350, 100));
+		menubutton.setFont(new Font("고딕", Font.BOLD, 60));
+		menubutton.setForeground(Color.white);
+		menubutton.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "메뉴 주문 페이지로 이동 합니다.");			
+			}});
+		
+		
+		this.add(leftPanel);
+		this.add(rightPanel);
+		mf.add(this);
+	}
+		
+}
+
