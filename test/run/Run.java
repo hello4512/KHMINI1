b6c46548968cabd0a90b2f85dee2c09e029111ad
orzsasin <80640024+orzsasin@users.noreply.github.com>
2021-08-06 14:08:57 +0900

    로그인 후 화면 버튼 누르면 이동하게 해놨습니다
    이동

---
+++ test/run/Run.java
@@ -0,0 +1,18 @@
+package test.com.kh.test.run;
+
+import test.com.kh.test.view.MainFrame;
+import test.com.kh.test.view.ManagerPage;
+import test.com.kh.test.view.Selectplace;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		// LoginAfterPage lap = new LoginAfterPage();
+		// ManagerPage mp = new ManagerPage();
+		new MainFrame();
+		// Selectplace sp = new Selectplace();
+		
+	}
+
+}
---
+++ test/src/com/kh/test/run/Run.java
@@ -0,0 +1,16 @@
+package com.kh.test.run;
+
+import com.kh.test.view.MainFrame;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		// LoginAfterPage lap = new LoginAfterPage();
+		// ManagerPage mp = new ManagerPage();
+		new MainFrame();
+		// Selectplace sp = new Selectplace();
+		
+	}
+
+}
